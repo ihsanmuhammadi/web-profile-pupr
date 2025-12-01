915856eb70d01980bf2c72aebf38b90edfc10ba5
Ihsan Muhammad Iqbal <ihsanmiqbal24@gmail.com>
2025-12-02 00:08:32 +0700

    set env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 
 
